52130468007a02bf72d7eff5a7262425cbf20adc
Mitra Davoudi <55147150+mitra-tech@users.noreply.github.com>
2025-05-03 22:25:39 -0600

    add: prisma neon file

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,14 +1,15 @@
-import { Pool, neonConfig } from "@neondatabase/serverless";
+import { neonConfig } from "@neondatabase/serverless";
+import { Pool as NeonPool } from "@neondatabase/serverless"; // ✅ Forces correct Pool type
+
 import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@/generated/prisma";
 import ws from "ws";
 
 // Sets up WebSocket connections, which enables Neon to use WebSocket communication.
 neonConfig.webSocketConstructor = ws;
-const connectionString = `${process.env.DATABASE_URL}`;
 
 // Creates a new connection pool using the provided connection string, allowing multiple concurrent connections.
-const pool = new Pool({ connectionString });
+const pool = new NeonPool({ connectionString: process.env.DATABASE_URL! });
 
 // Instantiates the Prisma adapter using the Neon connection pool to handle the connection between Prisma and Neon.
 const adapter = new PrismaNeon(pool);
