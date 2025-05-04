cd1b73a92add520cd1894847e6b77b6407e76fb8
Mitra Davoudi <55147150+mitra-tech@users.noreply.github.com>
2025-05-04 12:44:50 -0600

    add: product details page

--- src/lib/actions/product.actions.ts
+++ src/lib/actions/product.actions.ts
@@ -2,6 +2,7 @@
 
 import { PrismaClient } from "@/generated/prisma";
 import { convertToPlainObject } from "../utils";
+
 // what prisma client returns is an object and we need to convert it to a palain JS object
 
 // get latest products
@@ -15,4 +16,14 @@ export async function getLatestProducts() {
   });
   // we got an error aboutthe type of data object for not being a JS object (it was a prisma client object) then we passed the data to convertToPlainObject to conver it to plain JS object
   return convertToPlainObject(data);
+}
+
+// get single product by its slug
+export async function getProductBySlug(slug: string) {
+  const prisma = new PrismaClient();
+
+  // find the first slug where the slug is equal to the slug is passed in
+  return await prisma.product.findFirst({
+    where: { slug: slug },
+  });
 }
