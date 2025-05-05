62e600741d11a589136362e3282d83ad1a68538d
Mitra Davoudi <55147150+mitra-tech@users.noreply.github.com>
2025-05-05 10:33:38 -0600

    add: auth options

--- src/types/index.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-import { insertProductSchema } from "@/lib/validators";
-
-export type Product = z.infer<typeof insertProductSchema> & {
-  id: string;
-  rating: string;
-  createdAt: Date;
-};
