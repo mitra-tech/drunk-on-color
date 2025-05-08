9d852ef683af6a8276eaf00d6ffb60d600da4d85
Mitra Davoudi <55147150+mitra-tech@users.noreply.github.com>
2025-05-08 09:05:47 -0600

    add: format errors function for handlimg errors from Zod and Prisma

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -16,3 +16,25 @@ export function formatNumberWithDecimal(num: number): string {
   const [int, decimal] = num.toString().split(".");
   return decimal ? `${int}.${decimal.padEnd(2, "0")}` : `${int}.00`;
 }
+
+// Format Errors
+// We can add a type from prisma and zod, bur for now we ignore the any type
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function formatErrors(error: any) {
+  if (error.name === "ZodError") {
+    // Handle Zod error
+    // error.errors is array of objects and the key is 0 for the first one and 1 for the second one
+    const fieldErrors = Object.keys(error.errors).map(
+      // error.errors[field] => one of the objects of the array
+      (field) => error.errors[field].message
+    );
+    return fieldErrors.join(".");
+  } else if (
+    error.name === "PrismaClientKnownRequestError" &&
+    error.code === "P2002"
+  ) {
+    // Handle prisma error
+  } else {
+    // Handle other errors
+  }
+}
