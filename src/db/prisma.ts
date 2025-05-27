5901613d36ad0b2834d9f1f6a5854f9b752629c3
Mitra Davoudi <55147150+mitra-tech@users.noreply.github.com>
2025-05-27 11:31:14 -0600

    change: prisma client

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,28 +1,25 @@
 import { neonConfig } from "@neondatabase/serverless";
 import { PrismaNeon } from "@prisma/adapter-neon";
-import { PrismaClient } from "../../generated/prisma";
+import { PrismaClient } from "@prisma/client";
 import ws from "ws";
+import { Product } from "@/generated/prisma";
 
-// Sets up WebSocket connections, which enables Neon to use WebSocket communication.
 neonConfig.webSocketConstructor = ws;
+
 const connectionString = `${process.env.DATABASE_URL}`;
 
-// Creates a new connection pool using the provided connection string, allowing multiple concurrent connections.
-
-// Instantiates the Prisma adapter using the Neon connection pool to handle the connection between Prisma and Neon.
 const adapter = new PrismaNeon({ connectionString });
 
-// Extends the PrismaClient with a custom result transformer to convert the price and rating fields to strings.
 export const prisma = new PrismaClient({ adapter }).$extends({
   result: {
     product: {
       price: {
-        compute(product) {
+        compute(product: Product) {
           return product.price.toString();
         },
       },
       rating: {
-        compute(product) {
+        compute(product: Product) {
           return product.rating.toString();
         },
       },
