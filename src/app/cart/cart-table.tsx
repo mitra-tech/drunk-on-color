b7113e873485ca051d3e402f547b7ba9b79bb5a8
Mitra Davoudi <55147150+mitra-tech@users.noreply.github.com>
2025-06-04 10:47:28 -0600

    add: cart table part 1

--- src/app/cart/cart-table.tsx
+++ src/app/cart/cart-table.tsx
@@ -1,5 +1,32 @@
-const CartTable = () => {
-  return <>Cart Table</>;
+"use client";
+
+import { useRouter } from "next/navigation";
+import { Cart } from "@/types";
+import { toast } from "sonner";
+import { useTransition } from "react";
+import { addItemToCart } from "@/lib/actions/cart.actions";
+import { removeItemFromCart } from "@/lib/actions/cart.actions";
+import { ArrowRight, Loader, Minus, Plus } from "lucide-react";
+import Link from "next/link";
+import Image from "next/image";
+
+const CartTable = ({ cart }: { cart?: Cart }) => {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  return (
+    <>
+      <h1 className="py-4 h2-bold">Shopping Cart</h1>
+      {!cart || cart.items.length === 0 ? (
+        <div>
+          Cart is empty. <Link href="/">Go Shopping</Link>
+        </div>
+      ) : (
+        <div className="grid md:grid-cols-4 md:gap-5">
+          <div className="overflow-x-auto md:col-span-3">Table</div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default CartTable;
--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,13 +1,15 @@
 import CartTable from "./cart-table";
+import { getMyCart } from "@/lib/actions/cart.actions";
 
 export const metadata = {
   title: "Shopping Cart",
 };
 
-const CartPage = () => {
+const CartPage = async () => {
+  const cart = await getMyCart();
   return (
     <>
-      <CartTable />
+      <CartTable cart={cart} />
     </>
   );
 };
