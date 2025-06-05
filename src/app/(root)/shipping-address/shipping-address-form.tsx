042572e2fb06260bda384da7fa8526822ddbd931
Mitra Davoudi <55147150+mitra-tech@users.noreply.github.com>
2025-06-05 11:11:23 -0600

    add: form to shipping address form

--- src/app/(root)/shipping-address/shipping-address-form.tsx
+++ src/app/(root)/shipping-address/shipping-address-form.tsx
@@ -5,9 +5,61 @@ import { useTransition } from "react";
 import { shippingAddressDefaultValues } from "@/lib/constants";
 import { shippingAddressSchema } from "@/lib/validators";
 import { ShippingAddress } from "@/types";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ControllerRenderProps, Form, useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 const ShippingAddressForm = ({ address }: { address: ShippingAddress }) => {
-  return <>Shipping Address Form</>;
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof shippingAddressSchema>>({
+    resolver: zodResolver(shippingAddressSchema),
+    defaultValues: address || shippingAddressDefaultValues,
+  });
+
+  const [isPending, startTransition] = useTransition();
+
+  return (
+    <>
+      <div className="max-w-md mx-auto space-y-4">
+        <h1 className="h2-bold mt-4">Shipping Address</h1>
+        <p className="text-sm text-muted-foreground">
+          Please enter and address to ship to
+        </p>
+        <Form {...form}>
+          <form
+            action="post"
+            className="space-y-4"
+            onSubmit={form.handleSubmit(onSubmit)}
+          >
+            <div className="flex flex-col md:flex-row gap-5">
+              <FormField
+                control={form.control}
+                name="fullName"
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <FormLabel>Full Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter full name" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </form>
+        </Form>
+      </div>
+    </>
+  );
 };
 
 export default ShippingAddressForm;
