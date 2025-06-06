80ecc643167d275538cf22814ced13000d586e49
Mitra Davoudi <55147150+mitra-tech@users.noreply.github.com>
2025-06-06 16:06:21 -0600

    add: type validators for order and orderitem

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,6 +4,8 @@ import {
   insertProductSchema,
   cartItemSchema,
   shippingAddressSchema,
+  insertOrderItemSchema,
+  insertOrderSchema,
 } from "@/lib/validators";
 
 export type Product = z.infer<typeof insertProductSchema> & {
@@ -15,3 +17,16 @@ export type Product = z.infer<typeof insertProductSchema> & {
 export type Cart = z.infer<typeof insertCartSchema>;
 export type CartItem = z.infer<typeof cartItemSchema>;
 export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
+export type OrderItem = z.infer<typeof insertOrderItemSchema>;
+// the things that we have added after & is the items that are in the db table, but not in the form
+// so we have to add them here
+export type Order = z.infer<typeof insertOrderSchema> & {
+  id: string;
+  createdAt: Date;
+  isPaid: boolean;
+  PaidAt: Date | null;
+  isDelivered: boolean;
+  deliveredAt: Date | null;
+  orderItems: OrderItem[];
+  user: { name: string; email: string };
+};
