5af62d53576a0276ec74101814aebed568b4e512
Mitra Davoudi <55147150+mitra-tech@users.noreply.github.com>
2025-06-10 19:00:30 -0600

    add: paypal capture payment ethod

--- src/lib/paypal.ts
+++ src/lib/paypal.ts
@@ -24,6 +24,19 @@ export const paypal = {
     });
     return handleResponse(response);
   },
+  capturePayment: async function capturePayment(orderId: string) {
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders/${orderId}/capture`;
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    return handleResponse(response);
+  },
 };
 
 // Generate paypal access token
