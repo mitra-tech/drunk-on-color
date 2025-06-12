bfb72dffc0e657fcde9c1f522cbc4e703fd7aa2c
Mitra Davoudi <55147150+mitra-tech@users.noreply.github.com>
2025-06-12 10:31:19 -0600

    add: update user profile action and schema

--- src/lib/validators.ts
+++ src/lib/validators.ts
@@ -115,9 +115,16 @@ export const insertOrderItemSchema = z.object({
   qty: z.number(),
 });
 
+// Schema for user paument result
 export const paymentResultSchema = z.object({
   id: z.string(),
   status: z.string(),
   email_address: z.string(),
   pricePaid: z.string(),
+});
+
+// Schema for updating the user profile
+export const updateProfilesSchema = z.object({
+  name: z.string().min(3, "Name must be at least 3 characters"),
+  email: z.string().min(3, "Email must be at least 3 characters"),
 });
