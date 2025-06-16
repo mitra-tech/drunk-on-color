387e7a02d89dbe39ea3c5b2666f54849c538023f
Mitra Davoudi <55147150+mitra-tech@users.noreply.github.com>
2025-06-16 12:49:29 -0600

    add: get all products for admin action

--- src/app/admin/overview/page.tsx
+++ src/app/admin/overview/page.tsx
@@ -13,6 +13,7 @@ import { BadgeDollarSign, Barcode, CreditCard, Users } from "lucide-react";
 import { Metadata } from "next";
 import Link from "next/link";
 import Charts from "./charts";
+import { requireAdmin } from "@/lib/auth-guard";
 
 export const metadata: Metadata = {
   title: "Admin Dashboard",
@@ -20,6 +21,7 @@ export const metadata: Metadata = {
 
 const AdminOverviewPage = async () => {
   const summary = await getOrderSummary();
+  await requireAdmin();
 
   return (
     <div className="space-y-2">
--- src/lib/actions/product.actions.ts
+++ src/lib/actions/product.actions.ts
@@ -1,7 +1,8 @@
 "use server";
 import { prisma } from "@/db/prisma";
 import { convertToPlainObject } from "../utils";
-
+import { Prisma } from "@prisma/client";
+import { PAGE_SIZE } from "../constants";
 // what prisma client returns is an object and we need to convert it to a palain JS object
 
 // get latest products
@@ -22,4 +23,84 @@ export async function getProductBySlug(slug: string) {
   return await prisma.product.findFirst({
     where: { slug: slug },
   });
+}
+
+// Get all products
+export async function getAllProducts({
+  query,
+  limit = PAGE_SIZE,
+  page,
+  category,
+  price,
+  rating,
+  sort,
+}: {
+  query: string;
+  limit?: number;
+  page: number;
+  category?: string;
+  price?: string;
+  rating?: string;
+  sort?: string;
+}) {
+  // Query filter
+  const queryFilter: Prisma.ProductWhereInput =
+    query && query !== "all"
+      ? {
+          name: {
+            contains: query,
+            mode: "insensitive",
+          } as Prisma.StringFilter,
+        }
+      : {};
+
+  // Category filter
+  const categoryFilter = category && category !== "all" ? { category } : {};
+
+  // Price filter
+  const priceFilter: Prisma.ProductWhereInput =
+    price && price !== "all"
+      ? {
+          price: {
+            gte: Number(price.split("-")[0]),
+            lte: Number(price.split("-")[1]),
+          },
+        }
+      : {};
+
+  // Rating filter
+  const ratingFilter =
+    rating && rating !== "all"
+      ? {
+          rating: {
+            gte: Number(rating),
+          },
+        }
+      : {};
+
+  const data = await prisma.product.findMany({
+    where: {
+      ...queryFilter,
+      ...categoryFilter,
+      ...priceFilter,
+      ...ratingFilter,
+    },
+    orderBy:
+      sort === "lowest"
+        ? { price: "asc" }
+        : sort === "highest"
+        ? { price: "desc" }
+        : sort === "rating"
+        ? { rating: "desc" }
+        : { createdAt: "desc" },
+    skip: (page - 1) * limit,
+    take: limit,
+  });
+
+  const dataCount = await prisma.product.count();
+
+  return {
+    data,
+    totalPages: Math.ceil(dataCount / limit),
+  };
 }
