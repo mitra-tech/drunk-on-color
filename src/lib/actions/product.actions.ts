9c35cfae82b499182ebb4fba26c0cfd2564ed01a
Mitra Davoudi <55147150+mitra-tech@users.noreply.github.com>
2025-06-18 14:02:50 -0600

    add: update a product action

--- src/lib/actions/product.actions.ts
+++ src/lib/actions/product.actions.ts
@@ -4,7 +4,7 @@ import { convertToPlainObject, formatError } from "../utils";
 import { Prisma } from "@prisma/client";
 import { PAGE_SIZE } from "../constants";
 import { revalidatePath } from "next/cache";
-import { insertProductSchema } from "../validators";
+import { insertProductSchema, updateProductSchema } from "../validators";
 import z from "zod";
 
 // what prisma client returns is an object and we need to convert it to a palain JS object
@@ -142,6 +142,32 @@ export async function createProduct(data: z.infer<typeof insertProductSchema>) {
     return {
       success: true,
       message: "Product created successfully",
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
+
+// Update a product
+export async function updateProduct(data: z.infer<typeof updateProductSchema>) {
+  try {
+    const product = updateProductSchema.parse(data);
+    const productExists = await prisma.product.findFirst({
+      where: { id: product.id },
+    });
+
+    if (!productExists) throw new Error("Product not found");
+
+    await prisma.product.update({
+      where: { id: product.id },
+      data: product,
+    });
+
+    revalidatePath("/admin/products");
+
+    return {
+      success: true,
+      message: "Product updated successfully",
     };
   } catch (error) {
     return { success: false, message: formatError(error) };
