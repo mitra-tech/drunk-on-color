71508003acd9596ea84f5ddc80e60f1b091cf2fc
Mitra Davoudi <55147150+mitra-tech@users.noreply.github.com>
2025-06-18 16:57:25 -0600

    add: product form part-2

--- src/components/admin/product-form.tsx
+++ src/components/admin/product-form.tsx
@@ -18,9 +18,7 @@ import {
 } from "../ui/form";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
-import { createProduct, updateProduct } from "@/lib/actions/product.actions";
-import { Card, CardContent } from "../ui/card";
-import Image from "next/image";
+import { createProduct } from "@/lib/actions/product.actions";
 
 const ProductForm = ({
   type,
@@ -46,7 +44,88 @@ const ProductForm = ({
         : (productDefaultValues as InsertFormType),
   });
 
-  return <Form {...form}>Product Form</Form>;
+  const onSubmit: SubmitHandler<z.infer<typeof insertProductSchema>> = async (
+    values
+  ) => {
+    // On Create
+    if (type === "Create") {
+      const res = await createProduct(values);
+
+      if (!res.success) {
+        toast.error(res.message);
+      } else {
+        toast.success(res.message);
+
+        router.push("/admin/products");
+      }
+    }
+
+    // On Update
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        method="POST"
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8"
+      >
+        <div className="flex flex-col md:flex-row gap-5">
+          {/* Name */}
+          <FormField
+            control={form.control}
+            name="name"
+            render={({
+              field,
+            }: {
+              field: ControllerRenderProps<
+                z.infer<typeof insertProductSchema>,
+                "name"
+              >;
+            }) => (
+              <FormItem className="w-full">
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter product name" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* Slug */}
+          <FormField
+            control={form.control}
+            name="slug"
+            render={({
+              field,
+            }: {
+              field: ControllerRenderProps<
+                z.infer<typeof insertProductSchema>,
+                "slug"
+              >;
+            }) => (
+              <FormItem className="w-full">
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <div className="relative">
+                    <Input placeholder="Enter slug" {...field} />
+                    <Button
+                      type="button"
+                      className="bg-gray-500 hover:bg-gray-600 text-white px-4 py-1 mt-2"
+                      onClick={() => console.log("slogify")}
+                    >
+                      Generate
+                    </Button>
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+      </form>
+    </Form>
+  );
 };
 
 export default ProductForm;
