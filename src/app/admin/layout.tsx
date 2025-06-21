087bf36602bd667a8951b67528856fe6cc913351
Mitra Davoudi <55147150+mitra-tech@users.noreply.github.com>
2025-06-21 11:12:40 -0600

    add: admin layout menu

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -2,6 +2,8 @@ import { APP_NAME } from "@/lib/constants";
 import Image from "next/image";
 import Link from "next/link";
 import Menu from "@/components/shared/header/menu";
+import MainNav from "./main-nav";
+import AdminSearch from "@/components/admin/admin-search";
 
 export default function AdminLayout({
   children,
@@ -21,8 +23,9 @@ export default function AdminLayout({
                 alt={APP_NAME}
               />
             </Link>
-
+            <MainNav className="mx-6" />
             <div className="ml-auto items-center flex space-x-4">
+              <AdminSearch />
               <Menu />
             </div>
           </div>
